a33e1db8b0727fa46102ccd7b53c988ca31e025e
argianardi <argianardi1435@gmail.com>
2024-11-22 00:31:55 +0700

    Feat: Add header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
+import Header from "@/components/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start ">
-        <>
-          <button className="btn btn-primary">Hello daisyUI!</button>
-        </>
-      </main>
+    <div className="bb grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen  font-[family-name:var(--font-geist-sans)]">
+      <Header />
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start "></main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
