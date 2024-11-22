7bf59a78027ee1f4b1cd204dbcef9b7ba9af0780
argianardi <argianardi1435@gmail.com>
2024-11-22 08:56:19 +0700

    Chore: Add fake data

---
+++ src/constans/fakeData.ts
@@ -0,0 +1,127 @@
+export const highlightedCategories = [
+  "Oversized Hoodies",
+  "Crop Tops",
+  "BaggyJeans",
+  "Graphic Tees",
+  "Puffer Jackets",
+  "Wide-Leg Pants",
+  "Shackets",
+];
+
+export const productsCollection = [
+  {
+    image: "https://picsum.photos/200/300?random=1",
+    name: "Wireless Headphones",
+    price: 120000,
+    discountPercentage: 10,
+  },
+  {
+    image: "https://picsum.photos/200/300?random=2",
+    name: "Smartphone Stand",
+    price: 50000,
+    discountPercentage: 20,
+  },
+  {
+    image: "https://picsum.photos/200/300?random=3",
+    name: "Gaming Mouse",
+    price: 250000,
+    discountPercentage: 15,
+  },
+  {
+    image: "https://picsum.photos/200/300?random=4",
+    name: "Mechanical Keyboard",
+    price: 800000,
+    discountPercentage: 25,
+  },
+  {
+    image: "https://picsum.photos/200/300?random=5",
+    name: "LED Desk Lamp",
+    price: 150000,
+    discountPercentage: 5,
+  },
+  {
+    image: "https://picsum.photos/200/300?random=6",
+    name: "Bluetooth Speaker",
+    price: 180000,
+    discountPercentage: 30,
+  },
+  {
+    image: "https://picsum.photos/200/300?random=7",
+    name: "4K TV",
+    price: 5000000,
+    discountPercentage: 40,
+  },
+  {
+    image: "https://picsum.photos/200/300?random=8",
+    name: "Smart Watch",
+    price: 250000,
+    discountPercentage: 10,
+  },
+];
+
+export const fakeProductCategories = [
+  {
+    image: "https://picsum.photos/200/300?random=1",
+    name: "Electronics",
+  },
+  {
+    image: "https://picsum.photos/200/300?random=2",
+    name: "Fashion",
+  },
+  {
+    image: "https://picsum.photos/200/300?random=3",
+    name: "Home Appliances",
+  },
+  {
+    image: "https://picsum.photos/200/300?random=4",
+    name: "Books",
+  },
+  {
+    image: "https://picsum.photos/200/300?random=5",
+    name: "Beauty Products",
+  },
+  {
+    image: "https://picsum.photos/200/300?random=6",
+    name: "Sports",
+  },
+  {
+    image: "https://picsum.photos/200/300?random=7",
+    name: "Automotive",
+  },
+  {
+    image: "https://picsum.photos/200/300?random=8",
+    name: "Toys",
+  },
+  {
+    image: "https://picsum.photos/200/300?random=9",
+    name: "Furniture",
+  },
+  {
+    image: "https://picsum.photos/200/300?random=10",
+    name: "Groceries",
+  },
+  {
+    image: "https://picsum.photos/200/300?random=11",
+    name: "Pet Supplies",
+  },
+  {
+    image: "https://picsum.photos/200/300?random=12",
+    name: "Jewelry",
+  },
+  {
+    image: "https://picsum.photos/200/300?random=13",
+    name: "Gaming",
+  },
+  {
+    image: "https://picsum.photos/200/300?random=14",
+    name: "Health & Wellness",
+  },
+  {
+    image: "https://picsum.photos/200/300?random=15",
+    name: "Office Supplies",
+  },
+  {
+    image: "https://picsum.photos/200/300?random=16",
+    name: "Travel Accessories",
+  },
+];
