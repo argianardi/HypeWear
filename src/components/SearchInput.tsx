a4e1960a0304bed6fad7fa80631bfb064a46e094
argianardi <argianardi1435@gmail.com>
2024-11-22 08:53:54 +0700

    Style: Fix padding space

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BiSearch } from "react-icons/bi";
 
 const SearchInput = () => {
-  return <div>SearchInput</div>;
+  return (
+    <label className="input bg-OffWhite rounded-[36px] border-[#696969]/20 flex items-center gap-2">
+      <BiSearch size={20} />
+      <input
+        type="text"
+        className="grow placeholder:text-sm  placeholder:font-normal "
+        placeholder="Search"
+      />
+    </label>
+  );
 };
 
 export default SearchInput;
