3325a1502d12e7d734a560e3ca30587284a5faaa
argianardi <argianardi1435@gmail.com>
2024-11-25 05:57:40 +0700

    Styles: Update style to improve responsiveness

--- src/components/OurCollectionSection.tsx
+++ src/components/OurCollectionSection.tsx
@@ -2,10 +2,12 @@ import { productsCollection } from "@/constans/fakeData";
 import React from "react";
 import BasicProductCard from "./BasicProductCard";
 
-const OurCollection = () => {
+const OurCollectionSection = () => {
   return (
-    <div className="px-6 py-10 bg-OffWhite">
-      <h3 className="text-2xl font-bold text-CoolGray">Koleksi Kami</h3>
+    <div id="our-collection" className="px-6 py-10 bg-OffWhite">
+      <h3 className="text-xl lg:text-2xl font-bold text-CoolGray">
+        Koleksi Kami
+      </h3>
       <div className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {productsCollection?.map((product, index) => (
           <BasicProductCard
@@ -21,4 +23,4 @@ const OurCollection = () => {
   );
 };
 
-export default OurCollection;
+export default OurCollectionSection;
