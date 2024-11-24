f70901f8516c0a3742fb4163b390ce127e123ea5
argianardi <argianardi1435@gmail.com>
2024-11-25 05:56:07 +0700

    Styles: Update style to improve responsiveness

--- src/components/CategorySection.tsx
+++ src/components/CategorySection.tsx
@@ -4,25 +4,29 @@ import { fakeProductCategories } from "@/constans/fakeData";
 
 const CategorySection = () => {
   return (
-    <div className="py-10 bg-white overflow-x-hidden">
-      <h3 className="text-2xl font-bold text-CoolGray pl-6 mb-6">Kategori</h3>
-      <div className="flex gap-4">
-        {fakeProductCategories.map((category) => (
-          <CategoryCard
-            image={category.image}
-            name={category.name}
-            key={category.name}
-          />
-        ))}
-      </div>
-      <div className="flex gap-4 mt-4">
-        {fakeProductCategories.map((category) => (
-          <CategoryCard
-            image={category.image}
-            name={category.name}
-            key={category.name}
-          />
-        ))}
+    <div className="py-10 bg-white overflow-hidden">
+      <h3 className="text-xl 2xl:text-2xl font-bold text-CoolGray pl-6 mb-6">
+        Kategori
+      </h3>
+      <div className="relative  group w-full h-[344px] 2xl:h-[504px]">
+        <div className="flex gap-4 absolute top-0 left-3 animate-scroll-to-left">
+          {fakeProductCategories.map((category, index) => (
+            <CategoryCard
+              image={category.image}
+              name={category.name}
+              key={index}
+            />
+          ))}
+        </div>
+        <div className="flex absolute right-3 bottom-0 gap-4 group-hover:mt-20 transform duration-300 animate-scroll-to-right mt-4">
+          {fakeProductCategories.map((category, index) => (
+            <CategoryCard
+              image={category.image}
+              name={category.name}
+              key={index}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
