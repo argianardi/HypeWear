84252f960e33b5ba0925f098843152311847a71b
argianardi <argianardi1435@gmail.com>
2024-11-26 00:16:23 +0700

    Style: Fix card overlay

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-
 const CategoryCard = ({ image, name }: { image: string; name: string }) => {
   return (
     <div
-      className="rounded-lg hero h-40 hover:z-[9999] 2xl:h-60 w-[320px] xl:w-[480px] flex-none hover:scale-125 transition-transform duration-300 ease-in-out cursor-pointer"
+      className="rounded-lg hero h-40 hover:z-[9999] 2xl:h-60 w-[320px] xl:w-[480px] flex-none  transition-transform duration-300 ease-in-out cursor-pointer relative flex justify-center items-center"
       style={{
         backgroundImage: `url(${image})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
       }}
     >
-      <div className="rounded-lg overflow-hidden hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-neutral-content text-center">
-        <h2 className="text-2xl leading-[36px] text-white font-bold">{name}</h2>
-      </div>
+      <div className="rounded-lg bg-black/40 bg-opacity-60 absolute inset-0" />
+      <h2 className="text-2xl leading-[36px] text-white absolute font-bold">
+        {name}
+      </h2>
     </div>
   );
 };
