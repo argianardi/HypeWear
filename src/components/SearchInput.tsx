6d590831d81fb4c514d7f606de24e82b505439bd
argianardi <argianardi1435@gmail.com>
2024-11-26 00:19:16 +0700

    Style: Fix backgroud and border style

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,13 +1,12 @@
-import React from "react";
 import { BiSearch } from "react-icons/bi";
 
 const SearchInput = () => {
   return (
-    <label className="input bg-OffWhite rounded-[36px] border-[#696969]/20 flex items-center gap-2">
-      <BiSearch size={20} />
+    <label className="input border border-AshGray/20 px-3 lg:px-4 py-2  text-xs lg:text-sm lg:py-3 w-full   bg-OffWhite rounded-full flex items-center gap-2">
+      <BiSearch size={20} className="text-gray-500" />
       <input
         type="text"
-        className="grow placeholder:text-sm placeholder:font-normal "
+        className="w-full placeholder:text-xs font-semibold lg:placeholder:text-sm bg-OffWhite placeholder:text-AshGray placeholder:font-normal focus:outline-none"
         placeholder="Search"
       />
     </label>
