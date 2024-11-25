a0599236cc6c1fad4dadc5403cc3bcddadd0dcf3
argianardi <argianardi1435@gmail.com>
2024-11-26 00:15:15 +0700

    Styles: Fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ import OurCollectionSection from "@/components/OurCollectionSection";
 
 export default function Home() {
   return (
-    <div className="overflow-x-hidden max-w-[1440px] mx-auto">
+    <div className="overflow-x-hidden max-w-[1920px] mx-auto">
       <Header />
-      <main className="mt-[125px]">
+      <main>
         <HeroSection />
         <FlashSaleSection />
         <CategorySection />
